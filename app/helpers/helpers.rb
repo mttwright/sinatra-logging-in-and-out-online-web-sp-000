db34310cb0604325622e2bf7aeb4b78fe31efb49
Matt Wright <mttwright@gmail.com>
2021-02-15 14:09:09 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+  
+  def current_user(session_hash)
+    User.find_by(id: session_hash[:user_id])
+  end
+  
+  def is_logged_in?(session_hash)
+    if session_hash[:user_id]
+      true
+    else
+      false
+    end
+  end
+  
+  
 end
